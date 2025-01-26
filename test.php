3b436f2915affe9d29d9a2d1b96c037954677a3d
Roberto Moreno <rampmaster@gmail.com>
2025-01-26 18:41:19 -0300

    fix: temporal classmap

--- test.php
+++ test.php
@@ -4,9 +4,9 @@ include 'vendor/autoload.php';
 
 use Rampmaster\EPub\Core\EPub;
 
-error_reporting(E_ALL | E_STRICT);
-ini_set('error_reporting', E_ALL | E_STRICT);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL | E_STRICT);
+//ini_set('error_reporting', E_ALL | E_STRICT);
+//ini_set('display_errors', 0);
 
 // Example.
 // Create a test book for download.
